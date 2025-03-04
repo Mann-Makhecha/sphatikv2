58663705630cb94561112629c2456446e184d665
Mann-Makhecha <mannmakhecha1506@gmail.com>
2025-03-04 19:47:35 +0530

    Enhance admin dashboard layout; add mobile button and course module sidebar, and include new admin CSS styles

--- dashboards/admin.php
+++ dashboards/admin.php
@@ -1,6 +1,7 @@
 <?php
 require_once '../includes/header.php';
 require_once '../includes/db.php';
+
 ?>
 
 <!DOCTYPE html>
@@ -9,8 +10,23 @@ require_once '../includes/db.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="<?= BASE_URL ?>/css/innerCourse.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/css/admin.css">
 </head>
 <body>
-    hello world!
+<button class="mobile-button"><svg xmlns="http://www.w3.org/2000/svg" height="2.2rem" viewBox="0 -960 960 960"
+                width="2.2rem" class="backSvg">
+                <path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z" />
+            </svg></button>
+    <main><div class="sidebar">
+                <h2>Course Modules</h2>
+                <ul>
+                    <li>Module 1</li>
+                    <li>Module 2</li>
+                    <li>Module 3</li>
+                </ul>
+            </div></main>
+            <script src="<?= BASE_URL ?>js/profile.js">
+        </script>
 </body>
 </html>
